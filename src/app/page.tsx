3c4a0186ae8d799f91cc3bdb34168d9179ba84a2
Pratham271 <pratham19csu221@ncuindia.edu>
2024-08-25 08:09:01 +0530

    testing updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center mt-10">
        <Button>Click me</Button>
     </div>
   );
